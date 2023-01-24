ea3540bb77d34e14d1f233e22924d8fcb510ca7e
Dev Agrawal <agrawadv@mail.uc.edu>
2023-01-24 00:22:41 -0500

    title and favicon

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 // @ts-expect-error no typing
 import vercel from "solid-start-vercel";
-  
+
 export default defineConfig(() => {
   return {
     plugins: [solid({ ssr: true, adapter: vercel({ edge: false }) })],
   };
 });
-  
